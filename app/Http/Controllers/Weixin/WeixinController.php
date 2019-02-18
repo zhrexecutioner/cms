f56e6fd1a816177e00b7b9090a02b75877f8a3c0
Your Name <3286401574@qq.com>
2019-02-18 18:14:51 +0800

    微信

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -14,12 +14,6 @@ class WeixinController extends Controller
 
     protected $redis_weixin_access_token = 'str:weixin_access_token';     //微信 access_token
 
-    public function test()
-    {
-        //echo __METHOD__;
-        //$this->getWXAccessToken();
-        $this->getUserInfo(1);
-    }
 
     /**
      * 首次接入
@@ -31,58 +25,6 @@ class WeixinController extends Controller
         //file_put_contents('logs/weixin.log',$str,FILE_APPEND);
         echo $_GET['echostr'];
     }
-
-    /**
-     * 接收微信服务器事件推送
-     */
-    public function wxEvent()
-    {
-        $data = file_get_contents("php://input");
-
-
-        //解析XML
-        $xml = simplexml_load_string($data);        //将 xml字符串 转换成对象
-
-        $event = $xml->Event;                       //事件类型
-        //var_dump($xml);echo '<hr>';
-
-        if($event=='subscribe'){
-            $openid = $xml->FromUserName;               //用户openid
-            $sub_time = $xml->CreateTime;               //扫码关注时间
-
-
-            echo 'openid: '.$openid;echo '</br>';
-            echo '$sub_time: ' . $sub_time;
-
-            //获取用户信息
-            $user_info = $this->getUserInfo($openid);
-            echo '<pre>';print_r($user_info);echo '</pre>';
-
-            //保存用户信息
-            $u = WeixinUser::where(['openid'=>$openid])->first();
-            //var_dump($u);die;
-            if($u){       //用户不存在
-                echo '用户已存在';
-            }else{
-                $user_data = [
-                    'openid'            => $openid,
-                    'add_time'          => time(),
-                    'nickname'          => $user_info['nickname'],
-                    'sex'               => $user_info['sex'],
-                    'headimgurl'        => $user_info['headimgurl'],
-                    'subscribe_time'    => $sub_time,
-                ];
-
-                $id = WeixinUser::insertGetId($user_data);      //保存用户信息
-                var_dump($id);
-            }
-        }
-
-        $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
-        file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
-    }
-
-
 
 
     /**
@@ -120,21 +62,7 @@ class WeixinController extends Controller
 
     }
 
-    /**
-     * 获取用户信息
-     * @param $openid
-     */
-    public function getUserInfo($openid)
-    {
-        //$openid = 'oLreB1jAnJFzV_8AGWUZlfuaoQto';
-        $access_token = $this->getWXAccessToken();
-        $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
-
-        $data = json_decode(file_get_contents($url),true);
-        //echo '<pre>';print_r($data);echo '</pre>';
-        return $data;
-    }
-
+ 
 
 
 
