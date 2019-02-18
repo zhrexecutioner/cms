b72d600547c4cbe43669bc638f5b694eb22b67a6
Your Name <3286401574@qq.com>
2019-02-18 18:11:45 +0800

    微信

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,8 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        '/test/*'
+        '/test/*',
+        '/weixin/valid1',
+        '/weixin/valid'
     ];
 }
