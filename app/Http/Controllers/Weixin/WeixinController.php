59645bbed3e17140bd199438db2f1db4eb66b382
Your Name <3286401574@qq.com>
2019-02-19 14:55:23 +0800

    xx

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -86,6 +86,8 @@ class WeixinController extends Controller
         // 文本消息
         $xml_response = '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$from.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['. 'Hello World, 现在时间'. date('Y-m-d H:i:s') .']]></Content></xml>';
         echo $xml_response;
+
+        
     }
 
 
