8d4cc86a64f4e80da8c7289494ade8093894491f
Your Name <3286401574@qq.com>
2019-02-26 15:38:33 +0800

    x

--- app/Http/Controllers/Weixin/PayController.php
+++ app/Http/Controllers/Weixin/PayController.php
@@ -12,7 +12,7 @@ class PayController extends Controller
     //
 
     public $weixin_unifiedorder_url = 'https://api.mch.weixin.qq.com/pay/unifiedorder';
-    public $weixin_notify_url = 'http://shop.comcto.com/weixin/pay/notice';     //支付通知回调
+    public $weixin_notify_url = 'http://zhr.phpclub.top/weixin/pay/notice';     //支付通知回调
 
     public function test()
     {
