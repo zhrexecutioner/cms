4aeed6107d24719e7babbeccacaf3b34c73563b5
Your Name <3286401574@qq.com>
2019-02-26 15:09:22 +0800

    x

--- routes/web.php
+++ routes/web.php
@@ -181,8 +181,13 @@ Route::get('/weixin/kefu/chat','Weixin\WeixinController@chatView');     //客服
 Route::get('/weixin/chat/get_msg','Weixin\WeixinController@getChatMsg');     //获取用户聊天信息
 
 
+//微信支付
 Route::get('/weixin/pay/test','Weixin\PayController@test');     //微信支付测试
-Route::post('/weixin/pay/notice','Weixin\PayController@notice');     //微信支付
+Route::post('/weixin/pay/notice','Weixin\PayController@notice');     //微信支付通知回调
+
+
+
+
 
 
 
