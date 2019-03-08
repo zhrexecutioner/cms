c08b1d91a596540ef4dfca1280d6d45eacd36328
Your Name <3286401574@qq.com>
2019-03-08 11:10:02 +0800

    x

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -649,17 +649,33 @@ public function createMenuexam(Request $request){
         $client = new GuzzleHttp\Client(['base_uri' => $url]);
 
         $data = [
-            "button"    => [
-                [
-                    "name"  => "$name1",
-                    "sub_button"   => 
-                    [
-                        "type" => "$type",
-                        "name" => "$name2",
-                        "url" => "$urla"
-                    ]
-                ]
-            ],
+            "button":[
+     {    
+          "type":"click",
+          "name":"今日歌曲",
+          "key":"V1001_TODAY_MUSIC"
+      },
+      {
+           "name":"菜单",
+           "sub_button":[
+           {    
+               "type":"view",
+               "name":"搜索",
+               "url":"http://www.soso.com/"
+            },
+            {
+                 "type":"miniprogram",
+                 "name":"wxa",
+                 "url":"http://mp.weixin.qq.com",
+                 "appid":"wx286b93c14bbf93aa",
+                 "pagepath":"pages/lunar/index"
+             },
+            {
+               "type":"click",
+               "name":"赞一下我们",
+               "key":"V1001_GOOD"
+            }]
+       }]
         ];
 
 
