1c13c8777af63d5422222a1ab1fe2534b2d0aa7c
Your Name <3286401574@qq.com>
2019-03-08 10:07:25 +0800

    x

--- resources/views/weixin/exam.blade.php
+++ resources/views/weixin/exam.blade.php
@@ -1,7 +1,7 @@
 <script src="http://zhr.phpclub.top/js/jquery-1.11.2.min.js"></script>
 <form action="" class="form-inline">
     <button class="first" id="first">一级按钮</button>名字：<input type="text" id="name1"><button class="clone1" id="clone1">克隆</button><br>
-    <button class="second" id="second" display="none">二级按钮</button><button class="clone2" id="clone2">克隆</button><br>
+    <input class="second" id="second" typoe="button" display="none">二级按钮<button class="clone2" id="clone2">克隆</button><br>
     按钮类型：<select name="" id="">
 				<option value="click">click</option>
 				<option value="view">view</option>
