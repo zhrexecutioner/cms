f617f3d3ac07a822fcc31d8a3963d395d026ec74
Your Name <3286401574@qq.com>
2019-03-08 09:11:07 +0800

    x

--- resources/views/weixin/exam.blade.php
+++ resources/views/weixin/exam.blade.php
@@ -1,4 +1,12 @@
 <form action="" class="form-inline">
-    <button class="first" id="first">一级按钮</button>名字：<input type="text" id="first"><button class="clone1" id="clone1">克隆</button>
-    <button class="second" id="second">二级按钮</button><button class="clone2" id="clone2">克隆</button>
+    <button class="first" id="first">一级按钮</button>名字：<input type="text" id="name1"><button class="clone1" id="clone1">克隆</button><br>
+    <button class="second" id="second">二级按钮</button><button class="clone2" id="clone2">克隆</button><br>
+    按钮类型：<select name="" id="">
+				<option value="click">click</option>
+				<option value="view">view</option>
+    		 </select><br>
+    二级按钮名字：<input type="text" id="name2"><br>
+    二级按钮url：<input type="text" id="url"><br>
+    二级按钮名字key：<input type="text" id="key"><br>
+    <button class="submit" id="submit">提交</button>
 </form>
