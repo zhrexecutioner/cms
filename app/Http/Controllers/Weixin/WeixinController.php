e2253558537faa65e9654653b7895d161d2000d4
Your Name <3286401574@qq.com>
2019-03-08 11:20:00 +0800

    x

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -650,11 +650,6 @@ public function createMenuexam(Request $request){
 
         $data = [
             "button" =>[
-             [    
-                  "type" => "click",
-                  "name" => "今日歌曲",
-                  "key" => "V1001_TODAY_MUSIC"
-              ],
               [
                    "name" => "菜单",
                    "sub_button" => [
