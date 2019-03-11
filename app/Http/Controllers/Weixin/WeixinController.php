495b303b63b335a589f06cf55da0dd3441721b2c
Your Name <3286401574@qq.com>
2019-03-11 09:13:49 +0800

    x

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -32,9 +32,9 @@ class WeixinController extends Controller
      */
     public function validToken1()
     {
-        //$get = json_encode($_GET);
-        //$str = '>>>>>' . date('Y-m-d H:i:s') .' '. $get . "<<<<<\n";
-        //file_put_contents('logs/weixin.log',$str,FILE_APPEND);
+        $get = json_encode($_GET);
+        $str = '>>>>>' . date('Y-m-d H:i:s') .' '. $get . "<<<<<\n";
+        file_put_contents('logs/weixin.log',$str,FILE_APPEND);
         echo $_GET['echostr'];
     }
 
