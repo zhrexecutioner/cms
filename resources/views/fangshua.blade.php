9389cea0dfa812ca4977017cb49ace22c40c263c
Your Name <3286401574@qq.com>
2019-04-30 11:42:19 +0800

    x

--- resources/views/fangshua.blade.php
+++ resources/views/fangshua.blade.php
@@ -1,6 +1,6 @@
 <form action="/fangshua" method="post">
 	{{csrf_field()}}
-	<input type="text" name="fangshuatime">超时时间</br>
-	<input type="text" name="fangshuanum">频率</br>
+	<input type="text" name="fangshuatime">超时时间
+	<input type="text" name="fangshuanum">频率
 	<button type="submit">提交</button>
 </form>
--- resources/views/shijian.blade.php
+++ resources/views/shijian.blade.php
@@ -1,5 +1,5 @@
 <form action="/shijian" method="post">
 	{{csrf_field()}}
-	<input type="text" name="time">过期时间</br>
+	<input type="text" name="time">过期时间
 	<button type="submit">提交</button>
 </form>
